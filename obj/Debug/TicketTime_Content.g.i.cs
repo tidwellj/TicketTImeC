3cda61388227eb243203fa4f3dcbca9d0fd5b22b
Jason Tidwell <68177963+tidwellj@users.noreply.github.com>
2024-02-13 14:58:52 -0600

    Minor change
    Created new icon and added license file.

--- obj/Debug/TicketTime_Content.g.i.cs
+++ obj/Debug/TicketTime_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ticket.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ticket.png")]
 
 
